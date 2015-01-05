ee3f349556dde207c42cd1f5b97743dd898e6d54
Alex Dutton <alex.dutton@duttonsoftware.com>
2015-01-04 19:47:28 -0800

    Get connection between client/server
    Upgrade to Python 2.7 for deployment
    Add first pictures for logo
    Add preliminary questions

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from google.appengine.ext import db
 import webapp2
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+from questions import questions
 
 def getRoomNumber():
     letters = string.ascii_uppercase
@@ -30,7 +31,9 @@ class Messager(object):
 
   def Send(self, message):
     """Send a message to the client associated with the user."""
-    channel.send_message(self.unique_id, json.dumps(message))
+    payload = json.dumps(message, default=lambda o: o.__dict__, sort_keys=True)
+    logging.info("Sending message to channel %s payload %s" % (self.unique_id, payload))
+    channel.send_message(self.unique_id, payload)
 
 
 class ConnectedPage(webapp2.RequestHandler):
@@ -38,7 +41,7 @@ class ConnectedPage(webapp2.RequestHandler):
 
   def post(self):
     messager = Messager(self.request.get("token"))
-    messager.Send("you be connected")
+    messager.Send(dict(message_type = "connected"))
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
@@ -55,9 +58,10 @@ class DisplayPage(webapp2.RequestHandler):
 
 class DisplayRoomPage(webapp2.RequestHandler):
   def get(self, room):
-    messager = Messager("myuniqueid4444")
+    # TODO: Validate that the room exists
+    messager = Messager(room + "-unique-token")
     channel_token = messager.CreateChannelToken()
-    template_values = { 'channel_token': channel_token }
+    template_values = { 'channel_token': channel_token, 'room': room.upper() }
     path = os.path.join(os.path.dirname(__file__), 'display.html')
     self.response.out.write(template.render(path, template_values))
 
@@ -72,19 +76,31 @@ class PlayerRoomPage(webapp2.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'player.html')
     self.response.out.write(template.render(path, template_values))
 
-application = webapp2.WSGIApplication([
+
+class RoomHandler(webapp2.RequestHandler):
+  def post(self, room):
+    logging.info("Room status requested for %s by %s" % (room, self.request.get("token")))
+    messager = Messager(self.request.get("token"))
+    messager.Send(GetRoomStatus(room))
+
+def GetRoomStatus(room):
+    return RoomStatus(room)
+
+class RoomStatus(object):
+    def __init__(self, room_id):
+        self.message_type = "room"
+        self.room_id = room_id
+        self.players = [random.choice(["Alex", "Fred", "Emily", "George"])]
+        self.status = "waiting"
+
+app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/display/?', DisplayPage),
     ('/display/([A-Za-z]+)', DisplayRoomPage),
     ('/player/?', PlayerPage),
     ('/player/([A-Za-z]+)', PlayerRoomPage),
+    ('/api/room/([A-Za-z]+)', RoomHandler),
     #('/setname', SetNamePage),
     ('/connected', ConnectedPage)
     ], debug=True)
 
-
-def main():
-  run_wsgi_app(application)
-
-if __name__ == "__main__":
-  main()
