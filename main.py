19476854d0d8f250db525edc9da131b0a237b63d
Alex Dutton <alex.dutton@duttonsoftware.com>
2015-01-11 21:14:39 -0800

    Add State check intervals

--- main.py
+++ main.py
@@ -20,13 +20,27 @@ DEBUG = False
 if os.environ.get('SERVER_SOFTWARE','').startswith('Development'):
     DEBUG = True
 
+class DateTimeJSONEncoder(json.JSONEncoder):
+    #def __init__(self):
+        #self.default = lambda o: o.__dict__
+        #self.sort_keys = True
+    def default(self, obj):
+        self.sort_keys = True
+        if isinstance(obj, datetime.datetime):
+            return obj.ctime() + ' UTC'
+        elif isinstance(obj, datetime.date):
+            return obj.isoformat() + ' UTC'
+        else:
+            ##return super(DateTimeJSONEncoder, self).default(obj)
+            return obj.__dict__
+
 class UserState(object):
     def __init__(self, nickname, is_ready):
         self.nickname = nickname
         self.is_ready = is_ready
 
     def __str__(self):
-        return '<%s: %s>' % (self.__class__.__name__, json.dumps(self, default=lambda o: o.__dict__, sort_keys=True))
+        return '<%s: %s>' % (self.__class__.__name__, json.dumps(self, cls=DateTimeJSONEncoder))
 
 class Room(object):
     def __init__(self):
@@ -35,53 +49,71 @@ class Room(object):
         self.status = "waiting"
         self.user_ids = []
         self.question=None
-        self.answers={}
+        self.guesses={}
+        self.host=None
+        self.time_to_switch = None
 
-    def set_answer(self,user_id,answer):
-        self.answers[user_id] = answer
-        if len(self.answers) == len(self.user_ids):
+    def set_guess(self,user_id,guess):
+        self.guesses[user_id] = guess
+        if len(self.guesses) == len(self.user_ids):
             # TODO: Do scoring here
             logging.info("Everyone has answered the question")
             self.status = "answeredquestion"
 
     def start_game(self):
-        self.status="started"
+        self.status="round"
         self.round=1
-        # HACK: Shortcutting the round process
-        self.status = 'askingquestion'
-        self.question=random.choice(questions)
-        self.answers={}
+        self.time_to_switch=datetime.datetime.now()+datetime.timedelta(0,5)
 
     def upsert_user(self, user_id):
         if not user_id in self.user_ids:
             self.user_ids.append(user_id)
 
+    def advance_state(self):
+        if self.time_to_switch < datetime.datetime.now():
+            if self.status == "round":
+                self.status = "questionguess"
+                self.time_to_switch = datetime.datetime.now()+datetime.timedelta(0,5)
+                self.guesses = {}
+                self.question = random.choice(questions)
+                self.save()
+            elif self.status=="questionguess":
+                self.status="questionanswer"
+                self.time_to_switch=datetime.datetime.now()+datetime.timedelta(0,5)
+                self.save()
+            elif self.status=="questionanswer":
+                self.status="questionreveal"
+                self.time_to_switch=datetime.datetime.now()+datetime.timedelta(0,5)
+                self.save()
+            elif self.status=="questionreveal":
+                self.status="questionscore"
+                self.time_to_switch=datetime.datetime.now()+datetime.timedelta(0,5)
+                self.save()
+            elif self.status=="questionscore":
+                self.status="score"
+                self.time_to_switch=datetime.datetime.now()+datetime.timedelta(0,5)
+                self.save()
+            elif self.status=="score":
+                self.status="round"
+                self.round = self.round + 1
+                self.time_to_switch=datetime.datetime.now()+datetime.timedelta(0,5)
+                self.save()
     def create_random_id(self):
         letters = string.ascii_uppercase
         result = random.choice(letters) + random.choice(letters) + random.choice(letters) + random.choice(letters)
         # TODO: Remove potentially obscene room names
         return result
 
-    def get_state(self):
-        users = [User.load(user_id) for user_id in self.user_ids]
-        return dict(
-            status = self.status,
-            users = [UserState(user.nickname, user.is_ready) for user in users],
-            round=self.round,
-            question = self.question.question if self.question else None,
-            answerers = self.answers.keys() if self.answers else [],
-            answers = [dict(nickname = user.nickname, answer = self.answers[user.user_id]) for user in users] if self.status == "answeredquestion" and self.answers else None
-        )
-
     @staticmethod
     def load(room_id):
         return memcache.get("room-" + room_id)
 
     def save(self):
+        self.host = self.user_ids[0] if len(self.user_ids) > 0 else None
         memcache.set("room-" + self.room_id, self)
 
     def __str__(self):
-        return '<%s: %s>' % (self.__class__.__name__, json.dumps(self, default=lambda o: o.__dict__, sort_keys=True))
+        return '<%s: %s>' % (self.__class__.__name__, json.dumps(self, cls=DateTimeJSONEncoder))
 
 class User(object):
     def __init__(self):
@@ -106,7 +138,7 @@ class User(object):
         memcache.set("user-" + self.user_id, self)
 
     def __str__(self):
-        return '<%s: %s>' % (self.__class__.__name__, json.dumps(self, default=lambda o: o.__dict__, sort_keys=True))
+        return '<%s: %s>' % (self.__class__.__name__, json.dumps(self, cls=DateTimeJSONEncoder))
 
 
 class ConnectedMessage(object):
@@ -118,17 +150,78 @@ class NewNicknameMessage(object):
         self.message_type = 'newnickname'
         self.newnickname = newnickname
 
-class RoomWaitingStateMessage(object):
-    def __init__(self, room_id, room_state):
+class RoomStateMessage(object):
+    def __init__(self, room):
         self.message_type = "room"
-        self.room_id = room_id
-        self.room = room_state
+        self.room_id = room.room_id
+        self.room = self.get_state(room)
 
-class RoomQuestionStateMessage(object):
-    def __init__(self, room_id):
-        self.message_type = "room"
-        self.status = "question"
-        self.question = temporary_question
+    def get_state(self,room):
+        users = [User.load(user_id) for user_id in room.user_ids]
+        switch_interval=(room.time_to_switch-datetime.datetime.now()).total_seconds()*1000.0 if room.time_to_switch else None
+
+
+        if room.status == 'waiting':
+            return dict(
+                room_id = room.room_id,
+                status = room.status,
+                users = [UserState(user.nickname, user.is_ready) for user in users],
+            )
+        elif room.status == "round":
+            return dict(
+                room_id = room.room_id,
+                status = room.status,
+                round = room.round,
+                time_to_switch = room.time_to_switch,
+                switch_interval = switch_interval
+            )
+        elif room.status == "questionguess":
+            return dict(
+                room_id = room.room_id,
+                status = room.status,
+                question = room.question.question,
+                time_to_switch = room.time_to_switch,
+                switch_interval = switch_interval
+            )
+        elif room.status == "questionanswer":
+            return dict(
+                room_id = room.room_id,
+                status = room.status,
+                guesses = ['TODO', 'LATER', 'Ill get er done'],
+                question = room.question.question,
+                time_to_switch = room.time_to_switch,
+                switch_interval = switch_interval
+            )
+        elif room.status == "questionreveal":
+            return dict(
+                room_id = room.room_id,
+                status = room.status,
+                guesses = [dict(answer=room.question.answer, guesssers=room.user_ids, is_correct=True)],
+                question = room.question.question,
+                time_to_switch = room.time_to_switch,
+                switch_interval = switch_interval
+            )
+        elif room.status == "questionscore":
+            return dict(
+                room_id = room.room_id,
+                status = room.status,
+                users = [UserState(user.nickname, user.is_ready) for user in users],
+                question = room.question.question,
+                time_to_switch = room.time_to_switch,
+                switch_interval = switch_interval
+            )
+        elif room.status == "score":
+            return dict(
+                room_id = room.room_id,
+                status = room.status,
+                users = [UserState(user.nickname, user.is_ready) for user in users],
+                time_to_switch = room.time_to_switch,
+                switch_interval = switch_interval
+            )
+
+            #answerers = self.guesses.keys() if self.guesses else [],
+            #guesses = [dict(nickname = user.nickname, guess = self.guesses[user.user_id]) for user in users] if self.status == "answeredquestion" and self.guesses else None
+
 
 
 class BaseRoomHandler(webapp2.RequestHandler):
@@ -152,7 +245,7 @@ class BaseRoomHandler(webapp2.RequestHandler):
     def send_messages(self):
         for user_id in self.messages:
             messages_to_send = self.messages[user_id]
-            payload = json.dumps(messages_to_send, default=lambda o: o.__dict__, sort_keys=True)
+            payload = json.dumps(messages_to_send, cls=DateTimeJSONEncoder)
             logging.info("Sending %d messages to %s" % (len(messages_to_send), user_id))
             channel.send_message(user_id, payload)
 
@@ -187,6 +280,27 @@ class IndexHandler(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
+class RoomCheckStateHandler(BaseRoomHandler):
+    def post(self, room_id):
+        room_id = room_id.upper()
+        (room, user) = self.get_room_and_user(room_id)
+        logging.info("Room state check requested for %s by %s" % (room_id, user.user_id))
+
+        if not room:
+            logging.warn("Room does not exist")
+            return
+
+        if room.host == user.user_id:
+            logging.info("You are the host "+ user.nickname)
+            room.advance_state()
+            self.add_room_message(room.room_id, RoomStateMessage(room))
+            self.send_messages()
+
+        else:
+            logging.warn("You're not the host!")
+
+
+
 class RoomConnectHandler(BaseRoomHandler):
     def post(self, room_id):
         room_id = room_id.upper()
@@ -201,7 +315,7 @@ class RoomConnectHandler(BaseRoomHandler):
         room.save()
 
         self.add_user_message(user.user_id, ConnectedMessage())
-        self.add_room_message(room.room_id, RoomWaitingStateMessage(room.room_id, room.get_state()))
+        self.add_room_message(room.room_id, RoomStateMessage(room))
         self.send_messages()
 
 class RoomSetNicknameHandler(BaseRoomHandler):
@@ -221,12 +335,27 @@ class RoomSetNicknameHandler(BaseRoomHandler):
         user.save()
 
         self.add_user_message(user.user_id, NewNicknameMessage(user.nickname))
-        self.add_room_message(room.room_id, RoomWaitingStateMessage(room.room_id, room.get_state()))
+        self.add_room_message(room.room_id, RoomStateMessage(room))
         self.send_messages()
 
 class RoomStartHandler(webapp2.RequestHandler):
     def post(self):
+        user_id = self.request.cookies.get("user_id")
+        logging.info("user_id from cookies:" + str(user_id))
+        user = None
+
+        if user_id:
+            user = User.load(user_id)
+            logging.info("loaded user from user_id:" + str(user))
+
+        if not user:
+            user = User()
+            user_id = user.user_id
+            user.save()
+
         room = Room()
+        # TODO: Figure out why the host doesn't get set right
+        room.host =  user.user_id
         room.save()
         url = self.request.host_url + "/room/" + room.room_id
         logging.info("Redirecting to %s", url)
@@ -263,36 +392,35 @@ class RoomStartGameHandler(BaseRoomHandler):
         room.start_game()
         room.save()
 
-        self.add_room_message(room.room_id, RoomWaitingStateMessage(room.room_id, room.get_state()))
+        self.add_room_message(room.room_id, RoomStateMessage(room))
         self.send_messages()
 
-class RoomSendAnswerHandler(BaseRoomHandler):
+class RoomSendGuessHandler(BaseRoomHandler):
     def post(self, room_id):
         room_id = room_id.upper()
         (room, user) = self.get_room_and_user(room_id)
-        answer = self.request.get('answer').upper()
-        logging.info("Answer %s received %s by %s" % (answer,room_id, user.user_id))
+        guess = self.request.get('guess').upper()
+        logging.info("Guess %s received %s by %s" % (guess,room_id, user.user_id))
 
 
         if not room:
             logging.warn("Room does not exist")
             return
 
-        room.set_answer(user.user_id, answer)
+        room.set_guess(user.user_id, guess)
         room.save()
 
-        self.add_room_message(room.room_id, RoomWaitingStateMessage(room.room_id, room.get_state()))
+        self.add_room_message(room.room_id, RoomStateMessage(room))
         self.send_messages()
 
-
-temporary_question = random.choice(questions).question
 
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
     ('/room/?', RoomStartHandler),
     ('/room/([A-Za-z]+)', RoomViewHandler),
+    ('/room/([A-Za-z]+)/checkstate', RoomCheckStateHandler),
     ('/room/([A-Za-z]+)/connect', RoomConnectHandler),
-    ('/room/([A-Za-z]+)/sendanswer', RoomSendAnswerHandler),
+    ('/room/([A-Za-z]+)/sendguess', RoomSendGuessHandler),
     ('/room/([A-Za-z]+)/setnickname', RoomSetNicknameHandler),
     ('/room/([A-Za-z]+)/startgame', RoomStartGameHandler)
     ], debug=True)
